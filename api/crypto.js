7473b0e78afb306ce6d90d8e3944fa2a10fd688c
A V. Resendiz <52089643+pytyN01@users.noreply.github.com>
2026-04-15 12:46:36 -0700

    Create crypto.js

---
+++ api/crypto.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  try {
+    const { page } = req.query;
+
+    const response = await fetch(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=${page}&sparkline=false`
+    );
+
+    const data = await response.json();
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
+}
