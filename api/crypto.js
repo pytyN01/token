53ab5293ec0546d4a1ddc0ad94d2ab9ceb8004f9
A V. Resendiz <52089643+pytyN01@users.noreply.github.com>
2026-04-15 12:55:46 -0700

    Update crypto.js

--- api/crypto.js
+++ api/crypto.js
@@ -1,14 +1,25 @@
 export default async function handler(req, res) {
   try {
-    const { page } = req.query;
+    const { page, ids } = req.query;
 
-    const response = await fetch(
-      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=${page}&sparkline=false`
-    );
+    let url = "https://api.coingecko.com/api/v3/coins/markets";
 
+    const params = new URLSearchParams({
+      vs_currency: "usd",
+      sparkline: "false",
+    });
+
+    if (ids) {
+      params.append("ids", ids);
+    } else {
+      params.append("order", "market_cap_desc");
+      params.append("per_page", "250");
+      params.append("page", page || "1");
+    }
+
+    const response = await fetch(`${url}?${params.toString()}`);
     const data = await response.json();
 
-    res.setHeader("Access-Control-Allow-Origin", "*");
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch data" });
